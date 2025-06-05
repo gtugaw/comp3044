9ea0d50a5c81d166e80c4134f8764882edd35473
gtugaw <tugawinga@gmail.com>
2025-06-05 18:40:37 +0000

    just another snapshot

--- myscript.sh
+++ myscript.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
 #this is a comment
 #obligatory hello world program
 echo "hello world"
+sleep 1
+echo "my working directory is "
+pwd
+sleep 1
+echo "the contents of my directory are "
+ls
